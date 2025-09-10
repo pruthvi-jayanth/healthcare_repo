e538f945698684a27ab524c49b44d47ba85201c1
pruthvi jayanth <pruthvi.jayanth15@gmail.com>
2025-09-10 05:33:02 +0000

    basic dbt stage transforms

--- models/staging/stg_patients.sql
+++ models/staging/stg_patients.sql
@@ -1,16 +1,27 @@
 {{config(
-    materialized= 'table',
+    materialized= 'incremental',
+    unique_key= 'patient_id',
+    incremental_strategy= 'merge',
+    on_schema_change= 'sink_all_columns',
     tags=['staging']
 )}}
+
 with source as (
-select * from {{source('healthcare','raw_patients')}}
+    select * from {{source('healthcare', 'raw_patients')}}
 )
 
-select 
-      "Patient ID" as patient_id,
-      "Patient Name" as patient_name,
-	  GENDER as gender,
-	  AGE as age,
-	  "City ID" as city_id,
-	  RACE as race
-from source      
+select
+    try_cast("Patient ID"as int) as patient_id,
+    "First Name" as first_name,
+    "Last Name" as last_name,
+    try_cast("Date of Birth" as date) as date_of_birth,
+    "Gender" as gender,
+    "City ID" as city_id,
+    try_cast("Loaded At" as timestamp) as loaded_at,
+    '{{ this.name }}' as _record_source,
+    current_timestamp() as _ingested_at
+from source
+
+{% if is_incremental() %}
+   where "Loaded At" > (select max("loaded_at") from {{ this }} )
+{% endif %}   
